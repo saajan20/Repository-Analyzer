24ac59317b0b0fdbad4e7b5c323e34f239509d10
Saajan Jha <saajanjha@Saajans-MacBook-Pro.local>
2025-09-24 15:49:16 +0530

    add generating postman....

--- github_analyzer_web.py
+++ github_analyzer_web.py
@@ -436,4 +436,4 @@ def download_original_readme():
         return f"Error downloading original README: {str(e)}", 500
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=8080, debug=True)
